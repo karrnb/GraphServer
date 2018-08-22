8dfbbd1726944e4b8a7bb9aecc2c7cfdbf450884
Karan Banga <karrnb@gmail.com>
2018-08-22 13:59:56 -0400

    first commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(qg^m=@8*a8y39bkik_rxi&6q^#tedz9&1tu@v=+b#vi855fe-'
+SECRET_KEY = 'mkd6)5x^!xz!daugi#%6go@(viz27@(g0h)3ib&bjubams(!6c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 # Application definition
